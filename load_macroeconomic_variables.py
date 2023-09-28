bd7eb205d95cdfcd88189e4f53a6f3db18ceb957
cye2020 <ariaplay101@gmail.com>
2023-09-29 07:43:21 +0900

    Change disease period

--- load_macroeconomic_variables.py
+++ load_macroeconomic_variables.py
@@ -65,8 +65,12 @@ if __name__ == '__main__':
     # disease: COVID-19 변수 추가
     data['disease'] = 0  # 모든 행에 대해 초기값을 0으로 설정
 
-    # 2020년도 이후의 날짜는 1로 설정
-    data.loc[data.index >= '2020-01-01', 'disease'] = 1
+    # 메르스 유행 기간은 1로 설정
+    data.loc[('2015-07-27' >= data.index) & (data.index >= '2015-05-20'), 'disease'] = 1
+    
+    # COVID-19기간은 1로 설정
+    data.loc[('2022-08-31' >= data.index) & (data.index >= '2020-01-20'), 'disease'] = 1
+    
 
     # 최종 병합된 데이터 저장
     data.to_csv('./data/kospi.csv')
