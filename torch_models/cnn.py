ab75478df3ba994adbd26472824c8e2eb9dd2635
cye2020 <ariaplay101@gmail.com>
2023-12-19 22:32:40 +0900

    CNN 수정

--- torch_models/cnn.py
+++ torch_models/cnn.py
@@ -1,63 +1,51 @@
 import torch
-import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class CNN(nn.Module):
-    def __init__(self, input_shape=(1, 7, 30),
-                conv_configs=[{'filters': 64, 'kernel_size': 2, 'dropout': 0}],
-                pool_size=2,
-                dense_configs=[{'neurons': 100, 'dropout': 0.25}, {'neurons': 30, 'dropout': 0}]):
+    def __init__(self, conv_layers, fc_layers):
         super(CNN, self).__init__()
 
-        # Create a list to store the layers
-        layers = []
+        self.convs = nn.ModuleList()
+        self.pools = nn.ModuleList()
+        for conv_layer in conv_layers:
+            self.convs.append(nn.Conv1d(conv_layer['input_channels'], conv_layer['output_channels'], conv_layer['kernel_size']))
+            self.pools.append(nn.MaxPool1d(conv_layer['pool_size']))
 
-        in_size = input_shape[1]
-        output_height = input_shape[2]
+        self.flatten = nn.Flatten()
         
-        for config in conv_configs:
-            layers.append(nn.Conv1d(in_channels=in_size, out_channels=config['filters'], kernel_size=config['kernel_size']))
-            layers.append(nn.ReLU())
-            layers.append(nn.MaxPool1d(kernel_size=pool_size))
-            if config['dropout'] > 0:
-                layers.append(nn.Dropout(config['dropout']))
-            in_size = config['filters']
-            output_height = ((output_height - config['kernel_size']) + 1) // pool_size
-        
-        in_size = in_size * output_height
-        # Flatten the output
-        layers.append(nn.Flatten())
-        for config in dense_configs:
-            layers.append(nn.Linear(in_features=in_size, out_features=config['neurons']))
-            layers.append(nn.ReLU())
-            if config['dropout'] > 0:
-                layers.append(nn.Dropout(config['dropout']))
-            in_size = config['neurons']
-        self.model = nn.Sequential(*layers)
-    
+        self.fcs = nn.ModuleList()
+        self.dropouts = nn.ModuleList()
+        for fc_layer in fc_layers:
+            self.fcs.append(nn.Linear(fc_layer['input_size'], fc_layer['output_size']))
+            self.dropouts.append(nn.Dropout(fc_layer['dropout_rate']))
+
+        self.out = nn.Linear(fc_layers[-1]['output_size'], 1)
+
     def forward(self, x):
-        return self.model(x)
+        for conv, pool in zip(self.convs, self.pools):
+            x = F.relu(conv(x))
+            x = pool(x)
 
-# 사용 예시
-if __name__ == '__main__':
-    conv_configs = [
-        {'filters': 64, 'kernel_size': 2, 'dropout': 0},
-        {'filters': 32, 'kernel_size': 2, 'dropout': 0}
-    ]
+        x = self.flatten(x)
 
-    dense_configs = [
-        {'neurons': 100, 'dropout': 0.25},
-        {'neurons': 50, 'dropout': 0.25},
-        {'neurons': 30, 'dropout': 0}
-    ]
+        for fc, dropout in zip(self.fcs, self.dropouts):
+            x = F.relu(fc(x))
+            x = dropout(x)
 
-    input_shape = (1, 5, 30)  # PyTorch의 입력 형식 (배치 크기, 채널 수, 시간 단계)
+        x = self.out(x)
+        return x
 
-    model = CNN(input_shape=input_shape, conv_configs=conv_configs, dense_configs=dense_configs)
-    print(model)
+conv_layers = [
+    {'input_channels': 7, 'output_channels': 64, 'kernel_size': 2, 'pool_size': 2},
+    {'input_channels': 64, 'output_channels': 128, 'kernel_size': 2, 'pool_size': 2}
+]
 
+fc_layers = [
+    {'input_size': 128 * 13, 'output_size': 100, 'dropout_rate': 0.25},
+    {'input_size': 100, 'output_size': 50, 'dropout_rate': 0.25}
+]
 
+model = CNN(conv_layers, fc_layers)
 
 
