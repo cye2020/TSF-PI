d974166798850050130e4bcb2e9bd730b9056a3f
cye2020 <ariaplay101@gmail.com>
2023-12-20 09:39:59 +0900

    add activation function 조정

--- torch_models/cnn.py
+++ torch_models/cnn.py
@@ -8,43 +8,48 @@ class CNN(nn.Module):
 
         self.convs = nn.ModuleList()
         self.pools = nn.ModuleList()
+        self.activations = nn.ModuleList()
         for conv_layer in conv_layers:
             self.convs.append(nn.Conv1d(conv_layer['input_channels'], conv_layer['output_channels'], conv_layer['kernel_size']))
             self.pools.append(nn.MaxPool1d(conv_layer['pool_size']))
+            self.activations.append(nn.ReLU() if conv_layer['activation'] == 'relu' else nn.Linear())
 
         self.flatten = nn.Flatten()
         
         self.fcs = nn.ModuleList()
         self.dropouts = nn.ModuleList()
+        self.fc_activations = nn.ModuleList()
         for fc_layer in fc_layers:
             self.fcs.append(nn.Linear(fc_layer['input_size'], fc_layer['output_size']))
             self.dropouts.append(nn.Dropout(fc_layer['dropout_rate']))
+            self.fc_activations.append(nn.ReLU() if fc_layer['activation'] == 'relu' else nn.Linear())
 
         self.out = nn.Linear(fc_layers[-1]['output_size'], 1)
 
     def forward(self, x):
-        for conv, pool in zip(self.convs, self.pools):
-            x = F.relu(conv(x))
+        for conv, pool, activation in zip(self.convs, self.pools, self.activations):
+            x = activation(conv(x))
             x = pool(x)
 
         x = self.flatten(x)
 
-        for fc, dropout in zip(self.fcs, self.dropouts):
-            x = F.relu(fc(x))
+        for fc, dropout, activation in zip(self.fcs, self.dropouts, self.fc_activations):
+            x = activation(fc(x))
             x = dropout(x)
 
         x = self.out(x)
         return x
 
+
 conv_layers = [
-    {'input_channels': 7, 'output_channels': 64, 'kernel_size': 2, 'pool_size': 2},
-    {'input_channels': 64, 'output_channels': 128, 'kernel_size': 2, 'pool_size': 2}
+    {'input_channels': 7, 'output_channels': 64, 'kernel_size': 2, 'pool_size': 2, 'activation': 'relu'}
 ]
 
 fc_layers = [
-    {'input_size': 128 * 13, 'output_size': 100, 'dropout_rate': 0.25},
-    {'input_size': 100, 'output_size': 50, 'dropout_rate': 0.25}
+    {'input_size': None, 'output_size': 100, 'activation': 'relu', 'dropout_rate': 0.25},
+    {'input_size': 100, 'output_size': 30, 'activation': 'linear'}
 ]
+
 
 model = CNN(conv_layers, fc_layers)
 
