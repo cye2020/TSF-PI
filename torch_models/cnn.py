85d7dbef86fbfb3ff53db83486f3a0ba4f88d1c4
cye2020 <ariaplay101@gmail.com>
2023-12-23 19:22:24 +0900

    init에서 첫 fc input size 계산

--- torch_models/cnn.py
+++ torch_models/cnn.py
@@ -7,29 +7,44 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class CNN(nn.Module):
     def __init__(self, conv_layers, fc_layers):
         super(CNN, self).__init__()
-        self.conv_layers = conv_layers
-        self.fc_layers = fc_layers
         
         self.convs = nn.ModuleList()
         self.pools = nn.ModuleList()
         self.activations = nn.ModuleList()
+        
+        output_size = None
         for conv_layer in conv_layers:
-            self.convs.append(nn.Conv1d(conv_layer['input_channels'], conv_layer['output_channels'], conv_layer['kernel_size']))
-            self.pools.append(nn.MaxPool1d(conv_layer['pool_size']))
-            self.activations.append(nn.ReLU() if conv_layer['activation'] == 'relu' else nn.Linear())
+            input_channels, input_length = conv_layer['input_shape']
+            output_channels = conv_layer['output_channels']
+            kernel_size = conv_layer['kernel_size']
+            pool_size = conv_layer['pool_size']
+            activation = conv_layer['activation']
+            
+            self.convs.append(nn.Conv1d(input_channels, output_channels, kernel_size))
+            self.pools.append(nn.MaxPool1d(pool_size))
+            self.activations.append(nn.ReLU() if activation == 'relu' else nn.Linear())
+            
+            conv_output_length = ((input_length - kernel_size) / 1) + 1  # stride is assumed to be 1
+            pool_output_length = conv_output_length // pool_size
+
+            output_size = output_channels * int(pool_output_length)
 
         self.flatten = nn.Flatten()
+        fc_layers[0]['input_size'] = output_size
         
         self.fcs = nn.ModuleList()
         self.dropouts = nn.ModuleList()
         self.fc_activations = nn.ModuleList()
-        for fc_layer in fc_layers[1:]:
-            self.fcs.append(nn.Linear(fc_layer['input_size'], fc_layer['output_size']))
-            self.dropouts.append(nn.Dropout(fc_layer['dropout_rate']) if 'dropout_rate' in fc_layer else None)
-            if 'activation' in fc_layer:
-                self.fc_activations.append(nn.ReLU() if fc_layer['activation'] == 'relu' else nn.Linear())
-            else:
-                self.fc_activations.append(None)
+        
+        for fc_layer in fc_layers:
+            input_size = fc_layer['input_size']
+            output_size = fc_layer['output_size']
+            activation = None if 'activation' not in fc_layer else nn.ReLU() if fc_layer['activation'] == 'relu' else nn.Linear()
+            dropout = nn.Dropout(fc_layer['dropout_rate']) if 'dropout_rate' in fc_layer else None
+            
+            self.fcs.append(nn.Linear(input_size, output_size))
+            self.dropouts.append(dropout)
+            self.fc_activations.append(activation)
 
         self.out = nn.Linear(fc_layers[-1]['output_size'], 1)
 
@@ -40,37 +55,28 @@ class CNN(nn.Module):
 
         x = self.flatten(x)
 
-        # Calculate the input size for the first fully connected layer
-        fc_input_size = x.size(1)
-    
-        # Then use this value to initialize the first fully connected layer
-        fc_layer = self.fc_layers[0]
-        
-        first_fc_layer = nn.Linear(fc_input_size, fc_layer['output_size']).to(device)
-        first_dropout_rate = nn.Dropout(fc_layer['dropout_rate']) if 'dropout_rate' in fc_layer else None
-        first_activation = None if 'activation' not in fc_layer else nn.ReLU() if fc_layer['activation'] == 'relu' else nn.Linear()
-        
-        self.fcs.insert(0, first_fc_layer)
-        self.dropouts.insert(0, first_dropout_rate)
-        self.fc_activations.insert(0, first_activation)
-
-    
+        print(self.fcs)
         for fc, dropout, activation in zip(self.fcs, self.dropouts, self.fc_activations):
             fc = fc.to(device)
+            
+            print(fc)
             x = fc(x)
             
-            if activation is not None:
+            if activation:
+                print(activation)
                 x = activation(x)
             
-            if dropout is not None:
+            if dropout:
                 x = dropout(x)
 
         x = self.out(x)
         return x
 
 
+
+# input_shape = (channel, length)
 conv_layers = [
-    {'input_channels': 7, 'output_channels': 64, 'kernel_size': 2, 'pool_size': 2, 'activation': 'relu'}
+    {'input_shape': (7, 30), 'output_channels': 64, 'kernel_size': 2, 'pool_size': 2, 'activation': 'relu'}
 ]
 
 fc_layers = [
@@ -82,3 +88,5 @@ fc_layers = [
 model = CNN(conv_layers, fc_layers)
 
 
+from torchsummary import summary
+summary(model, (7, 30), device='cuda' if torch.cuda.is_available() else 'cpu')
